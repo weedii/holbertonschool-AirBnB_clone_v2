5677eb50d8cec2d9fa3831721c8aa311f4a5f293
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-11 18:20:40 +0100

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# route page
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+# hbnb page
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+# <text> page
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_text(text):
+    s = ""
+    for i in text:
+        s += i if i != "_" else " "
+    return f"C {s}"
+
+# <text> with default page
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    return f"Python {text.replace('_', ' ')}"
+
+# number page
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
